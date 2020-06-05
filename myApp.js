410cdb129e426f4f26235dd78f25c2b9af3f7349
Isale <isale.bryan@gmail.com>
2020-06-05 10:22:57 +0300

    Get input from client - Route parameters

--- myApp.js
+++ myApp.js
@@ -70,6 +70,13 @@ app.get('/now',function (req, res, next) {
 
 
 /** 9)  Get input from client - Route parameters */
+app.get('/:word/echo', function (req, res) { 
+  // let word = requst.params.word
+  let {word} = req.params //destructuring
+  res.json({
+    echo: word
+  })
+ })
 
 
 /** 10) Get input from client - Query parameters */
